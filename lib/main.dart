5595a0d4064fd2746cd74ef3162efb606f14fab1
omrajkamat68 <ob2614@srmist.edu.in>
2025-04-19 21:56:03 +0530

    Login Page added , project created in firebase, packages added to use firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:recycle_app/pages/home.dart';
+import 'package:recycle_app/pages/login.dart';
 import 'package:recycle_app/pages/onboarding.dart';
 
 void main() {
@@ -33,7 +35,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Home()
+      home: LogIn()
     );
   }
 }
---
+++ lib/pages/login.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:recycle_app/services/widget_support.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Center(
+              child: Image.asset(
+                "images/login.png",
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Image.asset(
+              "images/recycle1.png",
+              height: 120,
+              width: 120,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              "Reduce, Reuse, Recycle.",
+              style: AppWidget.headlinetextstyle(25.0),
+            ),
+            Text("Repeat!", style: AppWidget.greentextstyle(32.0)),
+            SizedBox(height: 80.0),
+            Text(
+              "Every item you recycle\nmakes a difference!",
+              textAlign: TextAlign.center,
+              style: AppWidget.normaltextstyle(20.0),
+            ),
+            Text("Get Started!", style: AppWidget.greentextstyle(24.0)),
+            SizedBox(height: 30.0),
+            Container(
+              margin: EdgeInsets.only(left: 20.0, right: 20.0),
+
+              child: Material(
+                elevation: 4.0,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  padding: EdgeInsets.only(left: 20.0),
+
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(60),
+                        ),
+                        child: Image.asset(
+                          "images/google.png",
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(width: 20.0),
+                      Text(
+                        "Sign in with Google",
+                        style: AppWidget.whitetextstyle(25.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
