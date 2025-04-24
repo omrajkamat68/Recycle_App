37d82deb7aaa2aedb3aca971f6b651612bac3222
omrajkamat68 <ob2614@srmist.edu.in>
2025-04-24 09:50:55 +0530

    Admin approval page frontend added

---
+++ lib/Admin/admin_approval.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:recycle_app/services/widget_support.dart';
+
+class AdminApproval extends StatefulWidget {
+  const AdminApproval({super.key});
+
+  @override
+  State<AdminApproval> createState() => _AdminApprovalState();
+}
+
+class _AdminApprovalState extends State<AdminApproval> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 40.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
+                children: [
+                  Material(
+                    elevation: 3.0,
+                    borderRadius: BorderRadius.circular(60),
+                    child: Container(
+                      padding: EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                      child: Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width / 7),
+                  Text(
+                    "Admin Approval",
+                    style: AppWidget.headlinetextstyle(25.0),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                  color: Color(0xFFececf8),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 20.0,),
+                    Container(
+                      margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                      child: Material(
+                        elevation: 2.0,
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+                          child: Row(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(border: Border.all(color: Colors.black45, width: 2.0), borderRadius: BorderRadius.circular(20)),
+                                child: Image.asset(
+                                  "images/coca.png",
+                                  height: 120,
+                                  width: 120,
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                              SizedBox(width: 20.0,),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                Row(
+                                  children: [
+                                    Icon(Icons.person, color: Colors.green, size: 28.0,),
+                                    SizedBox(width: 10.0,),
+                                    Text("Omraj Kamat", style: AppWidget.normaltextstyle(20.0),)
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(Icons.location_on, color: Colors.green, size: 28.0,),
+                                    SizedBox(width: 10.0,),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width/3,
+                                      child: Text("Near Main Market, delhi", style: AppWidget.normaltextstyle(20.0),))
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(Icons.inventory, color: Colors.green, size: 28.0,),
+                                    SizedBox(width: 10.0,),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width/3,
+                                      child: Text("3", style: AppWidget.normaltextstyle(20.0),))
+                                  ],
+                                ),
+                                SizedBox(height: 5.0,),
+                                Container(
+                                  height: 40,
+                                  width: 180,
+                                  decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(10)),
+                                  child: Center(child: Text("Approve", style: AppWidget.whitetextstyle(20.0),)),
+                                )
+                              ],)
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:recycle_app/Admin/admin_approval.dart';
 import 'package:recycle_app/pages/home.dart';
 import 'package:recycle_app/pages/login.dart';
 import 'package:recycle_app/pages/onboarding.dart';
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Home()
+      home: AdminApproval()
     );
   }
 }
